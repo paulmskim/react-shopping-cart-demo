6e07d81cd72f5ee53864c7478590e23374a02069
paulmskim <kim.paul.ms@gmail.com>
2017-02-23 16:09:19 +0800

    change component order on item page

--- src/js/components/Item/ItemDetails.js
+++ src/js/components/Item/ItemDetails.js
@@ -3,20 +3,22 @@ import AddItem from './AddItem';
 
 const ItemDetails = ({ id, name, description, price }) => (
   <div className={'item item-' + id}>
-    <h1 className='item-name'>
-      {name}
-    </h1>
     <img
       className='item-image'
       src={'/assets/' + name.replace(/\s/g, '-').toLowerCase() + '.jpg'}
     />
-    <h2 className='item-price'>
-      ${price.toFixed(2)}
-    </h2>
-    <p className='item-desc'>
-      {description}
-    </p>
-    <AddItem id={id} />
+    <div className='item-details'>
+      <h1 className='item-name'>
+        {name}
+      </h1>
+      <h2 className='item-price'>
+        ${price.toFixed(2)}
+      </h2>
+      <p className='item-desc'>
+        {description}
+      </p>
+      <AddItem id={id} />
+    </div>
   </div>
 );
 
